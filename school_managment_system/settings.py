0af0451c81501b687c68fd7a71897875b9c1d87e
Muhammad Juman <muhammad1juman@gmail.com>
2023-06-14 23:15:43 +0500

    first commit

--- school_managment_system/settings.py
+++ school_managment_system/settings.py
@@ -83,23 +83,23 @@ WSGI_APPLICATION = 'school_managment_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'school_system',
-#         'USER': 'usamarehman',
-#         'PASSWORD': '12131415',
-#         'HOST': 'localhost',
-#         'PORT': '',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'school_system',
+        'USER': 'usamarehman',
+        'PASSWORD': '12131415',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
